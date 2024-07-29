012d35e18f6d2b8b18f29e341f3d35430bb53aac
ruanm <247987314@qq.com>
2024-07-29 17:09:16 +0800

    optimize listing ads component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
-import UploadThumbnail from "@/components/UploadThumbnail";
 import { Ad } from "@/models/Ad";
 import { useEffect, useState } from "react";
-import Link from "next/link";
+import AdItem from "@/components/AdItem";
 
 export default function Home() {
   const [ads, setAds] = useState<Ad[] | null>(null);
@@ -21,20 +20,7 @@ export default function Home() {
         <h2 className="font-bold mt-2 mb-4">Latest products</h2>
         <div className="grid md:grid-cols-4 gap-x-4 gap-y-6">
           {ads && ads.map(ad => (
-            <div
-              key={ad._id}
-              className="min-h-24 flex flex-col justify-start">
-                {ad.files?.length > 0 && (
-                  <div className="rounded-md overflow-hidden relative">
-                    <UploadThumbnail file={ad.files[0] } />
-                    <Link href={`/ad/${ad._id}`} className="absolute inset-0" />
-                  </div>
-                )}
-                <div>
-                  <p className="mt-1 font-bold">${ad.price}</p>
-                  <Link href={`/ad/${ad._id}`}>{ad.title}</Link>
-                </div>
-              </div>
+            <AdItem ad={ad} key={ad._id} />
           ))}
         </div>
       </div>
---
+++ src/components/AdItem.tsx
@@ -0,0 +1,24 @@
+
+import { Ad } from "@/models/Ad";
+import UploadThumbnail from "@/components/UploadThumbnail";
+import Link from 'next/link';
+
+
+const AdItem = ({ad}: {ad: Ad}) => {
+  return (
+    <div className="min-h-24 flex flex-col justify-start">
+      {ad.files?.length > 0 && (
+        <div className="rounded-md overflow-hidden relative">
+          <UploadThumbnail onClick={() => {}} file={ad.files[0] } />
+          <Link href={`/ad/${ad._id}`} className="absolute inset-0" />
+        </div>
+      )}
+      <div>
+        <p className="mt-1 font-bold">${ad.price}</p>
+        <Link href={`/ad/${ad._id}`}>{ad.title}</Link>
+      </div>
+    </div>
+  )
+}
+
+export default AdItem
