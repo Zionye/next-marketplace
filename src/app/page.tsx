06183ca6381473311263365c8c99f57f8f141565
ruanm <247987314@qq.com>
2024-08-14 17:48:49 +0800

    优化 searchform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { Ad } from "@/models/Ad";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import AdItem from "@/components/AdItem";
-import { categories } from "@/libs/helpers";
-import { faStore } from "@fortawesome/free-solid-svg-icons";
-import LabelRadioButton from "@/components/LabelRadioButton";
-import SubmitButton from "@/components/SubmitButton";
+import SearchForm from "@/components/SearchForm";
 
 export default function Home() {
   const [ads, setAds] = useState<Ad[] | null>(null);
-  const formRef = useRef<HTMLFormElement | null>(null);
 
   useEffect(() => {
     fetchAds();
@@ -41,46 +37,7 @@ export default function Home() {
 
   return (
     <div className="flex w-full">
-      <form 
-        ref={formRef}
-        action={handleSearch} 
-        className="bg-white grow w-1/4 p-4 border-r flex flex-col gap-4">
-          <input name="phrase" type="text" placeholder="Search Marketplace" />
-
-          <div className="flex flex-col gap-0">
-            <LabelRadioButton
-                name="category" 
-                value={''}
-                icon={faStore} 
-                onClick={()=>formRef.current?.requestSubmit()}
-                label={'All categories'}
-                defaultChecked={true}/>
-
-            {categories.map(({key:categoryKey,label:categoryLabel, icon:categoryIcon}) => (
-              <LabelRadioButton 
-                key={'categoryKey'}
-                name="category" 
-                value={categoryKey} 
-                icon={categoryIcon} 
-                onClick={()=>formRef.current?.requestSubmit()}
-                label={categoryLabel}/>
-            ))}
-          </div>
-
-          <div>
-            <label>filter by price</label>
-            <div className="grid grid-cols-2 gap-4">
-              <div>
-                <input name="min" type="number" placeholder="min"/>
-              </div>
-              <div>
-                <input name="max" type="number" placeholder="max"/>
-              </div>
-            </div>
-          </div>
-
-          <SubmitButton>Search</SubmitButton>
-      </form>
+      <SearchForm action={handleSearch} />
 
       <div className="bg-gray-100 p-4 grow w-3/4">
         <h2 className="font-bold mt-2 mb-4">Latest products</h2>
